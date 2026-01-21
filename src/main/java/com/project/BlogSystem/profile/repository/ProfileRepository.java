6b2b914a4f6ef222398d4aa9df0a79aa9f8933c6
r9988.2588 <r9988.2588@gmail.com>
2026-01-21 03:41:38 +0000

    feat(profile): add existsByUser method

--- src/main/java/com/project/BlogSystem/profile/repository/ProfileRepository.java
+++ src/main/java/com/project/BlogSystem/profile/repository/ProfileRepository.java
@@ -3,6 +3,7 @@ package com.project.BlogSystem.profile.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.project.BlogSystem.auth.model.entity.AuthUser;
 import com.project.BlogSystem.profile.model.Profile;
 
 import java.util.Optional;
@@ -12,10 +13,13 @@ import java.util.Optional;
 Profile repository for database operations
 include method:
 * findByUser_Username(String username): finds the username of the particular user for the profile 
-
+* existsByUser(AuthUser user): check weather the user exists
 */
 
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
     Optional<Profile> findByUser_Username(String username);
+
+    boolean existsByUser(AuthUser user);
+
 }
