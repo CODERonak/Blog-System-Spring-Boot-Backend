a4dfc6bec462010b769cfc27693d128a795996fc
r9988.2588 <r9988.2588@gmail.com>
2026-01-23 05:19:03 +0000

    fix(post): change the method parameters from AuthenticatedUser to MultipartFile

--- src/main/java/com/project/BlogSystem/post/service/interfaces/PostService.java
+++ src/main/java/com/project/BlogSystem/post/service/interfaces/PostService.java
@@ -4,19 +4,18 @@ package com.project.BlogSystem.post.service.interfaces;
 import com.project.BlogSystem.post.dto.PostRequest;
 import com.project.BlogSystem.post.dto.PostResponse;
 import com.project.BlogSystem.post.model.enums.PostStatus;
-import com.project.BlogSystem.auth.security.AuthenticatedUser;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface PostService {
-    PostResponse createPost(PostRequest postRequest, AuthenticatedUser currentUser);
+    PostResponse createPost(PostRequest postRequest, MultipartFile file);
 
-    PostResponse updatePost(Long postId, PostRequest postRequest, AuthenticatedUser currentUser);
+    PostResponse updatePost(Long postId, PostRequest postRequest, MultipartFile file);
 
-    void deletePost(Long postId, AuthenticatedUser currentUser);
+    void deletePost(Long postId);
 
     Page<PostResponse> getPostsByUser(String name, Pageable pageable);
 
-    PostResponse changePostStatus(Long postId, PostStatus newStatus, AuthenticatedUser currentUser);
+    PostResponse changePostStatus(Long postId, PostStatus newStatus);
 }
-
